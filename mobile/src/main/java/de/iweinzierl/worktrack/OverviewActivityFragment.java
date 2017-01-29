2fcc3ecd809f8ae2133aeec4e5a47d36eb34ca76
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2017-01-29 14:58:32 +0100

    Project setup.

---
+++ mobile/src/main/java/de/iweinzierl/worktrack/OverviewActivityFragment.java
@@ -0,0 +1,22 @@
+package de.iweinzierl.worktrack;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class OverviewActivityFragment extends Fragment {
+
+    public OverviewActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+}
