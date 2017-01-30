2d527a6726279d8e4cd74326f8692986b415b211
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2017-01-30 22:53:35 +0100

    Added broadcast receiver to checkin and checkout.

---
+++ mobile/src/main/java/de/iweinzierl/worktrack/receiver/StartWorkReceiver.java
@@ -0,0 +1,39 @@
+package de.iweinzierl.worktrack.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.github.iweinzierl.android.logging.AndroidLoggerFactory;
+
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.EReceiver;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+
+import de.iweinzierl.worktrack.persistence.CreationType;
+import de.iweinzierl.worktrack.persistence.LocalTrackingItemRepository;
+import de.iweinzierl.worktrack.persistence.TrackingItem;
+import de.iweinzierl.worktrack.persistence.TrackingItemRepository;
+import de.iweinzierl.worktrack.persistence.TrackingItemType;
+
+@EReceiver
+public class StartWorkReceiver extends BroadcastReceiver {
+
+    private static final Logger LOGGER = AndroidLoggerFactory.getInstance().getLogger("StartWorkReceiver");
+
+    @Bean(LocalTrackingItemRepository.class)
+    TrackingItemRepository trackingItemRepository;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LOGGER.info("Received START_WORK broadcast");
+
+        TrackingItem item = new TrackingItem(TrackingItemType.CHECKIN, DateTime.now(), CreationType.AUTO);
+        TrackingItem save = trackingItemRepository.save(item);
+
+        if (save.getId() > 0) {
+            LOGGER.info("Successfully added tracking item after checkin broadcast event");
+        }
+    }
+}
