dd82e8fd01d3e6f98876dec2cf53192bded16e3d
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2017-02-26 20:58:32 +0100

    Switched charting library.

--- mobile/src/main/java/de/iweinzierl/worktrack/WeekOverviewFragment.java
+++ mobile/src/main/java/de/iweinzierl/worktrack/WeekOverviewFragment.java
@@ -5,14 +5,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
+import com.google.common.collect.Lists;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.ColorRes;
-import org.eazegraph.lib.charts.BarChart;
-import org.eazegraph.lib.models.BarModel;
+import org.androidannotations.annotations.res.StringRes;
 import org.joda.time.Duration;
 import org.joda.time.Hours;
 import org.joda.time.LocalDate;
@@ -26,6 +28,12 @@ import de.iweinzierl.worktrack.model.WeekDay;
 import de.iweinzierl.worktrack.persistence.LocalTrackingItemRepository;
 import de.iweinzierl.worktrack.persistence.TrackingItemRepository;
 import de.iweinzierl.worktrack.util.SettingsHelper;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.view.ColumnChartView;
 
 @EFragment(R.layout.fragment_week_overview)
 public class WeekOverviewFragment extends Fragment {
@@ -40,11 +48,19 @@ public class WeekOverviewFragment extends Fragment {
             .appendSuffix(" min ")
             .toFormatter();
 
+    @StringRes(R.string.activity_weekoverview_chart_xaxis_label)
+    String xAxisLabel;
+
+    @StringRes(R.string.activity_weekoverview_chart_yaxis_label)
+    String yAxisLabel;
+
     @Bean(LocalTrackingItemRepository.class)
     TrackingItemRepository trackingItemRepository;
 
     @ViewById
-    BarChart barChart;
+    ColumnChartView barChart;
+
+    ColumnChartData columnChartData;
 
     @ColorRes(R.color.barChartOverHours)
     int barColorOverHours;
@@ -68,6 +84,36 @@ public class WeekOverviewFragment extends Fragment {
     TextView durationView;
 
     public WeekOverviewFragment() {
+    }
+
+    @AfterViews
+    protected void initChart() {
+        Axis xAxis = new Axis();
+        xAxis.setHasLines(true);
+        xAxis.setHasSeparationLine(false);
+        xAxis.setHasTiltedLabels(true);
+        xAxis.setValues(Lists.newArrayList(
+                new AxisValue(0).setLabel(getString(R.string.monday)),
+                new AxisValue(1).setLabel(getString(R.string.tuesday)),
+                new AxisValue(2).setLabel(getString(R.string.wednesday)),
+                new AxisValue(3).setLabel(getString(R.string.thursday)),
+                new AxisValue(4).setLabel(getString(R.string.friday)),
+                new AxisValue(5).setLabel(getString(R.string.saturday)),
+                new AxisValue(6).setLabel(getString(R.string.sunday))
+        ));
+
+        Axis yAxis = new Axis();
+        yAxis.setHasLines(true);
+        yAxis.setHasSeparationLine(false);
+        yAxis.setHasTiltedLabels(true);
+
+        columnChartData = new ColumnChartData();
+        columnChartData.setAxisXBottom(xAxis);
+        columnChartData.setAxisYLeft(yAxis);
+        columnChartData.setStacked(false);
+
+        barChart.setColumnChartData(columnChartData);
+        barChart.setInteractive(false);
     }
 
     @Override
@@ -101,17 +147,24 @@ public class WeekOverviewFragment extends Fragment {
         Duration duration = new Duration(0);
         LocalDate date = null;
 
-        barChart.clearChart();
+        List<Column> columns = Lists.newArrayList();
 
         for (WeekDay day : weekDays) {
             long millis = day.getWorkingTime().getMillis();
             duration = duration.plus(millis);
 
             float hours = (float) ((int) (millis / (10 * 60 * 60))) / 100;
-            barChart.addBar(new BarModel(
-                    String.valueOf(day.getDate().toString("EEE")),
-                    hours,
-                    hours > dailyWorkingHours ? barColorOverHours : colorNormalHours));
+            SubcolumnValue subcolumnValue = new SubcolumnValue(hours);
+            subcolumnValue.setLabel(String.valueOf(hours));
+            subcolumnValue.setColor(
+                    hours > dailyWorkingHours
+                            ? barColorOverHours
+                            : colorNormalHours
+            );
+
+            Column column = new Column(Lists.newArrayList(subcolumnValue));
+            column.setHasLabels(true);
+            columns.add(column);
 
             if (!day.getItems().isEmpty()) {
                 date = day.getItems().get(0).getEventTime().toLocalDate();
@@ -130,7 +183,8 @@ public class WeekOverviewFragment extends Fragment {
             }
         }
 
-        barChart.startAnimation();
+        columnChartData.setColumns(columns);
+        barChart.setColumnChartData(columnChartData);
     }
 
     private String createWeekRangeString(LocalDate dayInWeek) {
