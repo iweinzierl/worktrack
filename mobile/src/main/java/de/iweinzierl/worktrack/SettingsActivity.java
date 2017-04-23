69ad4821219f271e1dac84baa42cfec4c76a8f32
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2017-04-23 12:25:40 +0200

    Made accounts selectable for backups.

--- mobile/src/main/java/de/iweinzierl/worktrack/SettingsActivity.java
+++ mobile/src/main/java/de/iweinzierl/worktrack/SettingsActivity.java
@@ -1,14 +1,22 @@
 package de.iweinzierl.worktrack;
 
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.PermissionChecker;
+
+import com.github.iweinzierl.android.logging.AndroidLoggerFactory;
 
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.FragmentById;
 import org.androidannotations.annotations.UiThread;
+import org.slf4j.Logger;
 
 import de.iweinzierl.worktrack.job.BackupJob;
 import de.iweinzierl.worktrack.model.BackupFrequency;
@@ -17,6 +25,12 @@ import de.iweinzierl.worktrack.util.BackupHelper;
 
 @EActivity
 public class SettingsActivity extends BaseGoogleApiActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final Logger LOGGER = AndroidLoggerFactory.getInstance().getLogger(SettingsActivity.class.getName());
+
+    public static final String ACCOUNT_TYPE_GOOGLE = "com.google";
+
+    private static final int REQUEST_GET_ACCOUNT_PERMISSION = 1001;
 
     @Override
     int getLayoutId() {
@@ -32,6 +46,8 @@ public class SettingsActivity extends BaseGoogleApiActivity implements SharedPre
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         preferences.registerOnSharedPreferenceChangeListener(this);
+
+        updateAccounts();
     }
 
     @Override
@@ -54,6 +70,29 @@ public class SettingsActivity extends BaseGoogleApiActivity implements SharedPre
             case "settings_backup_frequency":
                 backupFrequencyChanged(prefs.getString(key, null));
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_GET_ACCOUNT_PERMISSION && grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+            updateAccounts();
+        } else {
+            LOGGER.warn("GET_ACCOUNTS permission not granted!");
+        }
+    }
+
+    protected void updateAccounts() {
+        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) != PermissionChecker.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS}, REQUEST_GET_ACCOUNT_PERMISSION);
+            return;
+        }
+
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccountsByType(ACCOUNT_TYPE_GOOGLE);
+
+        settingsFragment.setAccounts(accounts);
     }
 
     @Background
--- mobile/src/main/java/de/iweinzierl/worktrack/SettingsFragment.java
+++ mobile/src/main/java/de/iweinzierl/worktrack/SettingsFragment.java
@@ -1,6 +1,8 @@
 package de.iweinzierl.worktrack;
 
+import android.accounts.Account;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 
 import org.androidannotations.annotations.EFragment;
@@ -14,6 +16,9 @@ public class SettingsFragment extends PreferenceFragment {
     @PreferenceByKey(R.string.preference_last_backup)
     LastBackupPreference lastBackupPreference;
 
+    @PreferenceByKey(R.string.preference_backup_account)
+    ListPreference accountPreference;
+
     public SettingsFragment() {
     }
 
@@ -25,5 +30,15 @@ public class SettingsFragment extends PreferenceFragment {
 
     public void setLastBackupSize(float kiloBytes) {
         lastBackupPreference.setLastBackupSize(kiloBytes);
+    }
+
+    public void setAccounts(Account[] accounts) {
+        CharSequence[] values = new CharSequence[accounts.length];
+        for (int i = 0; i < accounts.length; i++) {
+            values[i] = accounts[i].name;
+        }
+
+        accountPreference.setEntries(values);
+        accountPreference.setEntryValues(values);
     }
 }
